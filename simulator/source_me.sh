82ad8d9a050783772a8cc32ea54ff9306d690174
Terry Howe <tlhowe@amazon.com>
2020-12-13 15:22:47 -0700

    Update source me

--- simulator/source_me.sh
+++ simulator/source_me.sh
@@ -1,3 +1,4 @@
 export RPI_KERNEL=kernel-qemu-5.4.51-buster
+export RPI_DTB_FILE=versatile-pb-buster-5.4.51.dtb
 export RPI_IMG_DIR=raspios_lite_armhf-2020-12-04
 export RPI_IMG_NAME=2020-12-02-raspios-buster-armhf-lite
