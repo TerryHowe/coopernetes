8445735943dfc34f1d39d15a49bb7c6036cc90e0
Terry Howe <tlhowe@amazon.com>
2020-12-14 07:59:23 -0700

    Add dtb file to download

--- simulator/download_kernel.sh
+++ simulator/download_kernel.sh
@@ -1,2 +1,3 @@
-# Dowload kernel and export location
+# Dowload kernel and dtb file
 curl -OL https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/${RPI_KERNEL}
+curl -OL https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/${RPI_DTB_FILE}
