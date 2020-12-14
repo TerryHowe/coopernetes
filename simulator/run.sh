bc659f79e7114b108ecb4b6cb593dc321eb92358
Terry Howe <tlhowe@amazon.com>
2020-12-14 08:00:18 -0700

    Simplify rp run script

--- simulator/run.sh
+++ simulator/run.sh
@@ -1,17 +1,6 @@
-# Tweak filesystem: start qemu with init flag, switch to guest window to execute tweak and close window afterwards
 qemu-system-arm -kernel ./${RPI_KERNEL} \
--cpu arm1176 -m 256 \
--M versatilepb -no-reboot -serial stdio \
--append "root=/dev/sda2 panic=1 rootfstype=ext4 rw init=/bin/bash" \
--drive "file=./${RPI_IMG_NAME}.img,index=0,media=disk,format=raw"
-
-sed -i -e 's/^/#/' /etc/ld.so.conf
-sed -i -e 's/^/#/' /etc/fstab
-
-# Emulate Raspberry Pi
-qemu-system-arm -kernel ./${RPI_KERNEL} \
--cpu arm1176 -m 256 \
--M versatilepb -no-reboot -serial stdio \
--append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
--drive "file=./${RPI_IMG_NAME}.img,index=0,media=disk,format=raw"
--redir tcp:5022::22
+    -cpu arm1176 -m 256 -M raspi \
+    -dtb ${RPI_DTB_FILE} -no-reboot \
+    -serial stdio -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
+    -drive "file=${RPI_IMG_NAME}.img,index=0,media=disk,format=raw" \
+    -net user,hostfwd=tcp::5022-:22 -net nic
