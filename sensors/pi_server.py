f617bc099245c66288e58f4d0f75ab6dac1126bc
Terry Howe <tlhowe@amazon.com>
2020-12-19 07:45:10 -0700

    Add static favicon routes

--- sensors/pi_server.py
+++ sensors/pi_server.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 def index():
     parameters = {
         "title": get_hostname(),
-        "hostsname": get_hostname(),
+        "hostname": get_hostname(),
         "content": "<h1>Blah</h1>",
     }
     return render_template('index.html', **parameters)
@@ -40,6 +40,43 @@ def environment():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@app.route('/apple-touch-icon.png')
+def favicon_apple_touch():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'apple-touch-icon.png', mimetype='image/png')
+
+
+@app.route('/android-chrome-192x192.png')
+def favicon_192x192():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'android-chrome-192x192.png', mimetype='image/png')
+
+
+@app.route('/android-chrome-512x512.png')
+def favicon_512x512():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'android-chrome-512x512.png', mimetype='image/png')
+
+
+@app.route('/favicon-32x32.png')
+def favicon_32x32():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon-32x32.png', mimetype='image/png')
+
+
+@app.route('/favicon-16x16.png')
+def favicon_16x16():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon-16x16.png', mimetype='image/png')
+
+
+@app.route('/site.webmanifest')
+def webmanifest():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'site.webmanifest', mimetype=' application/manifest+json')
+
 
 @app.route('/style.css')
 def style_css():
