eb9f3961019748a118854888b57eefa7bf5b5715
Terry Howe <tlhowe@amazon.com>
2020-12-20 04:53:37 -0700

    Move  config.yaml out of the way

--- sensors/pi_config.py
+++ sensors/pi_config.py
@@ -1,9 +1,16 @@
+import os
 import yaml
 
 class PiConfig(object):
     def __init__(self):
         try:
-            with open("./config.yaml", 'r') as stream:
+            config_file = './config.yaml'
+            sample_config_file = './sample_config.yaml'
+            if (not os.path.exists(config_file) and
+                os.path.exists(sample_config_file)):
+                config_file = sample_config_file
+                print('Warning: Using {}'.format(config_file))
+            with open(config_file, 'r') as stream:
                 try:
                     self.config = yaml.safe_load(stream)
                     return
