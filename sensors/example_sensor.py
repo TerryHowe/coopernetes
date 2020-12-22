b2ff598367ba077c484bd3e8266ecc69ddfec9e6
Terry Howe <tlhowe@amazon.com>
2020-12-22 06:35:32 -0700

    Add prometheus to example sensor

--- sensors/example_sensor.py
+++ sensors/example_sensor.py
@@ -2,6 +2,8 @@
 
 import random
 
+from prometheus_client import Gauge
+
 from base_sensor import BaseSensor
 
 
@@ -10,9 +12,12 @@ class Sensor(BaseSensor):
     path = "example"
     image = "example_png"
     sample_rate = 10
+    g = Gauge('coopernetes_example_data', 'Coopernetes example data')
 
     def read_data(self):
+        value = round(123.456 + (random.random()*10), 1)
+        self.g.set(value)
         return {
             "timestamp": str(self.get_timestamp()),
-            "data": round(123.456 + (random.random()*10), 1),
+            "data": value,
         }
