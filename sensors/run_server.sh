5f57bff79e1ceef62748d9d9ba52b6cfddab81a3
Terry Howe <tlhowe@amazon.com>
2020-12-22 08:23:41 -0700

    Bind pi_server to all addresses

--- sensors/run_server.sh
+++ sensors/run_server.sh
@@ -4,4 +4,4 @@ BASEDIR=$(dirname "$0")
 cd ${BASEDIR}
 git pull 2>/dev/null || true
 VERSION=v$(git describe --tags --long)
-uwsgi --http 127.0.0.1:5000 --wsgi-file pi_server.py --pyarg "${VERSION}" --callable app
+uwsgi --http 0.0.0.0:5000 --wsgi-file pi_server.py --pyarg "${VERSION}" --callable app
