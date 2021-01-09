94dfb1ad7836e026d9b5af10c076616a589f545f
Terry Howe <tlhowe@amazon.com>
2021-01-09 09:20:22 -0700

    Update repo

--- ansible/roles/cluster-monitoring/files/deploy-monitoring.sh
+++ ansible/roles/cluster-monitoring/files/deploy-monitoring.sh
@@ -2,13 +2,11 @@
 
 set -e 
 
-git clone git@github.com:carlosedp/cluster-monitoring.git
+git clone git@github.com:TerryHowe/cluster-monitoring.git
 cd cluster-monitoring
 
 # Random SHA1 we have tested
 git checkout a136833aaf364a8e76809a9dca789bb59e070677
-
-git apply ../roles/cluster-monitoring/files/0001-Raspberry-changes.patch 
 
 make deploy
 cd ..
