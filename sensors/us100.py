e6a630d7188796fe332013a6ae115043f931410e
Terry Howe <tlhowe@amazon.com>
2021-01-20 04:45:17 -0700

    Implement us100 sensor

--- sensors/us100.py
+++ sensors/us100.py
@@ -1,39 +1,33 @@
 #!/usr/bin/env python3
 
-#from datetime import datetime, timezone
-
-import time
 import adafruit_us100
+from prometheus_client import Gauge
 import serial
 
-uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=3)
-print(uart)
-us100 = adafruit_us100.US100(uart)
-while True:
-    print("-----")
-    print("Temperature: ", us100.temperature)
-    print("Distance: ", us100.distance)
-    time.sleep(0.5)
+from base_sensor import BaseSensor
 
-#from prometheus_client import Gauge
-#
-#from base_sensor import BaseSensor
 
-#class Sensor(BaseSensor):
-#    description = "DHT22 Sensor"
-#    path = "dht22"
-#    image = "dht22_png"
-#    sample_rate = 30
-#    sensor = Adafruit_DHT.DHT22
-#    pin = 18
-#
-#    def __init__(self, **kwargs):
-#        super().__init__(**kwargs)
-#        self.timestamp = self.get_timestamp()
-#        name_prefix = self.hostname + '_dht22_' + str(self.pin)
-#        self.temperature = Gauge(name_prefix + '_temperature', 'Temperature')
-#        self.humidity = Gauge(name_prefix + '_humidity', 'Humidity Percent')
-#        self.last_result = Gauge(name_prefix + '_last_result', 'Last successful read')
-#
-#    def read_data(self):
-#        return
+class Sensor(BaseSensor):
+    description = "DHT22 Sensor"
+    path = "us100"
+    image = "us100_jpg"
+    sample_rate = 30
+    sensor = Adafruit_DHT.DHT22
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        name_prefix = self.hostname + '_us100'
+        self.distance = Gauge(name_prefix + '_humidity', 'Distance inches')
+        self.temperature = Gauge(name_prefix + '_temperature', 'Temperature')
+        self.uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=3)
+
+
+    def read_data(self):
+        try:
+            us100 = adafruit_us100.US100(uart)
+            self.temperature.set(round((((us100.temperature * 9.0) / 5.0) + 32), 1))
+            self.distance.set(round(us100.distance, 1))
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+        return
