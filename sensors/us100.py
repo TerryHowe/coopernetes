877797a6bbd1501a40b2aa4ad44122f84a459b46
Terry Howe <tlhowe@amazon.com>
2021-01-20 05:24:20 -0700

    Convert cm to in

--- sensors/us100.py
+++ sensors/us100.py
@@ -16,7 +16,7 @@ class Sensor(BaseSensor):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         name_prefix = self.hostname + '_us100'
-        self.distance = Gauge(name_prefix + '_humidity', 'Distance inches')
+        self.distance = Gauge(name_prefix + '_distance', 'Distance inches')
         self.temperature = Gauge(name_prefix + '_temperature', 'Temperature')
         self.uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=3)
 
@@ -25,7 +25,7 @@ class Sensor(BaseSensor):
         try:
             us100 = adafruit_us100.US100(self.uart)
             self.temperature.set(round((((us100.temperature * 9.0) / 5.0) + 32), 1))
-            self.distance.set(round(us100.distance, 1))
+            self.distance.set(round((us100.distance / 2.54), 1))
         except Exception as e:
             import traceback
             traceback.print_exc()
